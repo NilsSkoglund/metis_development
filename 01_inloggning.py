aa8d31a789fff546f279f1c5af5caa3a8195958b
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-03-03 07:03:40 +0100

    Update 01_inloggning.py

--- 01_inloggning.py
+++ 01_inloggning.py
@@ -10,48 +10,49 @@ dev_login_page.custom_authenticate()
 if st.session_state["authentication_status"]:
     # litet hack för bättre ux
     time.sleep(1)
-    # frontend
-
     st.session_state["authenticator"].logout('Logout', 'main')
     # litet hack för bättre ux
     time.sleep(1)
     st.write(f'Welcome {st.session_state["name"]}')
-
-    # backend
     # connect to database
-    # database name based on username - new session state variable
     st.session_state["db"] =\
     st.session_state["deta"].Base(st.session_state["username"])
-
     dev_login_page.custom_user_logged_in()
 
-# frontend
-    # fel inloggningsuppgifter
+# fel inloggningsuppgifter
 if st.session_state["authentication_status"] == False:
     st.error('Username/password is incorrect')
 
-    # frontend/backend
-    # användare väljar att registrera ny profil
-    # widget - new session state variable
-    st.checkbox("Registrera", key="register_user_v1")
-    if st.session_state["register_user_v1"]:
-        dev_login_page.custom_register_user()
-
-    st.checkbox("Glömt lösenord", key="forgot_pw_v1")
-    if st.session_state["forgot_pw_v1"]:
-        dev_login_page.custom_forgot_pw()
+    options = ["Logga in", "Registrera", "Glömt lösenord", "Glömt användarnamn"]
+    #labels = ["Registrera", "Glömt lösenord", "Glömt användarnamn"]
+    st.radio(options
+             , key="options_inloggning_1"
+             , horizontal=True)
     
-    st.checkbox("Glömt användarnamn", key="forgot_username_v1")
-    if st.session_state["forgot_username_v1"]:
+    if st.session_state["options_inloggning_1"] == "Registrera":
+        dev_login_page.custom_register_user()
+    if st.session_state["options_inloggning_1"] == "Glömt lösenord":
+        dev_login_page.custom_forgot_pw()
+    if st.session_state["options_inloggning_1"] == "Glömt användarnamn":
         dev_login_page.custom_forgot_username()
-# frontend
-    # ej angett inloggningsuppgifter
+
+
+    # st.checkbox("Registrera", key="register_user_v1")
+    # if st.session_state["register_user_v1"]:
+    #     dev_login_page.custom_register_user()
+
+    # st.checkbox("Glömt lösenord", key="forgot_pw_v1")
+    # if st.session_state["forgot_pw_v1"]:
+    #     dev_login_page.custom_forgot_pw()
+    
+    # st.checkbox("Glömt användarnamn", key="forgot_username_v1")
+    # if st.session_state["forgot_username_v1"]:
+    #     dev_login_page.custom_forgot_username()
+
+# ej angett inloggningsuppgifter
 if st.session_state["authentication_status"] == None:
     st.warning('Please enter your username and password')
 
-    # frontend/backend
-    # användare väljar att registrera ny profil
-    # widget - new session state variable
     st.checkbox("Registrera", key="register_user_v2")
     if st.session_state["register_user_v2"]:
         dev_login_page.custom_register_user()
