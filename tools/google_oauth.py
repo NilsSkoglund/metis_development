7c9ba0ef731cdd7f6157da758fa41ff45c50c54d
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-03-26 00:18:57 +0100

    Update google_oauth.py

--- tools/google_oauth.py
+++ tools/google_oauth.py
@@ -44,11 +44,9 @@ def login_button(authorization_url, app_name, app_desc):
     # st.markdown(container, unsafe_allow_html=True)
 
     container = f'''
-    <div class="text-center">
         <a target="_self" href="{authorization_url}"
             Logga in med Google
         </a>
-    </div>
     '''
     st.markdown(container, unsafe_allow_html=True)
 
