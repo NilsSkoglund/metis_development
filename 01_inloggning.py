42255494180974aead5af8a9d45253b3c4c15ed4
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-03-26 16:30:59 +0200

    Update 01_inloggning.py

--- 01_inloggning.py
+++ 01_inloggning.py
@@ -8,6 +8,20 @@ import extra_streamlit_components as stx
 from streamlit_lottie import st_lottie
 import requests
 from PIL import Image
+import base64
+from pathlib import Path
+
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
+def img_to_html(img_path):
+    img_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(
+      img_to_bytes(img_path)
+    )
+    return img_html
+
+st.markdown("<p style='text-align: center; color: grey;'>"+img_to_html('logo2.png')+"</p>", unsafe_allow_html=True)
 
 image = Image.open('logo2.png')
 
