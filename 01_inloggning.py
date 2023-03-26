5c5c9a36d3e7b7648d421160f09ca3d9bab82aa9
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-03-26 13:12:09 +0200

    Update 01_inloggning.py

--- 01_inloggning.py
+++ 01_inloggning.py
@@ -35,8 +35,6 @@ if st.session_state["authentication_status"] == None and "random_cookie_name" no
     if login_info:
         dev_login_page.custom_authenticate_oauth()
 
-dev_login_page.custom_authenticate()
-
 if st.session_state["authentication_status"]:
     # connect to database
     st.session_state["db"] =\
@@ -45,63 +43,68 @@ if st.session_state["authentication_status"]:
 
     dev_login_page.custom_user_logged_in()
 
-# fel inloggningsuppgifter
-if st.session_state["authentication_status"] == False:
-    st.error('Username/password is incorrect')
+with st.expander("Visa övriga alternativ"):
 
-    options = ["Logga in"
-               , "Registrera ny användare"
-               , "Glömt inloggningsuppgifter"]
-    st.radio("Välj meny"
-             , options
-             , key="options_inloggning_1"
-             , horizontal=False
-             , label_visibility="hidden")
-    
-    if st.session_state["options_inloggning_1"] == "Registrera ny användare":
-        dev_login_page.custom_register_user()
-    if st.session_state["options_inloggning_1"] == "Glömt inloggningsuppgifter":
-        st.write("---")
-        options_credentials = ["Glömt lösenord", "Glömt användarnamn"]
-        st.radio("Glömt inloggningsuppgifter"
-             , options_credentials
-             , key="options_credentials_1"
-             , horizontal=True
-             , label_visibility="visible")
-        if st.session_state["options_credentials_1"] == "Glömt lösenord":
-            st.info("Ange ditt användarnamn så skickas ett lösenord\
-                    till den kopplade emailen.")
-            dev_login_page.custom_forgot_pw()
-        if st.session_state["options_credentials_1"] == "Glömt användarnamn":
-            st.info("Ange den kopplade emailen så visas användarnamnet.")
-            dev_login_page.custom_forgot_username()
+    dev_login_page.custom_authenticate()
+
+
+    # fel inloggningsuppgifter
+    if st.session_state["authentication_status"] == False:
+        st.error('Username/password is incorrect')
+
+        options = ["Logga in"
+                , "Registrera ny användare"
+                , "Glömt inloggningsuppgifter"]
+        st.radio("Välj meny"
+                , options
+                , key="options_inloggning_1"
+                , horizontal=False
+                , label_visibility="hidden")
         
-    
+        if st.session_state["options_inloggning_1"] == "Registrera ny användare":
+            dev_login_page.custom_register_user()
+        if st.session_state["options_inloggning_1"] == "Glömt inloggningsuppgifter":
+            st.write("---")
+            options_credentials = ["Glömt lösenord", "Glömt användarnamn"]
+            st.radio("Glömt inloggningsuppgifter"
+                , options_credentials
+                , key="options_credentials_1"
+                , horizontal=True
+                , label_visibility="visible")
+            if st.session_state["options_credentials_1"] == "Glömt lösenord":
+                st.info("Ange ditt användarnamn så skickas ett lösenord\
+                        till den kopplade emailen.")
+                dev_login_page.custom_forgot_pw()
+            if st.session_state["options_credentials_1"] == "Glömt användarnamn":
+                st.info("Ange den kopplade emailen så visas användarnamnet.")
+                dev_login_page.custom_forgot_username()
+            
+        
 
-# ej angett inloggningsuppgifter
-if st.session_state["authentication_status"] == None:
-    options = ["Logga in", "Registrera ny användare",
-               "Glömt inloggningsuppgifter"]
-    st.radio("Välj meny"
-             , options
-             , key="options_inloggning_2"
-             , horizontal=False
-             , label_visibility="hidden")
-    
-    if st.session_state["options_inloggning_2"] == "Registrera ny användare":
-        dev_login_page.custom_register_user()
-    if st.session_state["options_inloggning_2"] == "Glömt inloggningsuppgifter":
-        st.write("---")
-        options_credentials = ["Glömt lösenord", "Glömt användarnamn"]
-        st.radio("Glömt inloggningsuppgifter"
-             , options_credentials
-             , key="options_credentials_2"
-             , horizontal=True
-             , label_visibility="visible")
-        if st.session_state["options_credentials_2"] == "Glömt lösenord":
-            st.info("Ange ditt användarnamn så skickas ett lösenord\
-                    till den kopplade emailen.")
-            dev_login_page.custom_forgot_pw()
-        if st.session_state["options_credentials_2"] == "Glömt användarnamn":
-            st.info("Ange den kopplade emailen så visas användarnamnet.")
-            dev_login_page.custom_forgot_username()
+    # ej angett inloggningsuppgifter
+    if st.session_state["authentication_status"] == None:
+        options = ["Logga in", "Registrera ny användare",
+                "Glömt inloggningsuppgifter"]
+        st.radio("Välj meny"
+                , options
+                , key="options_inloggning_2"
+                , horizontal=False
+                , label_visibility="hidden")
+        
+        if st.session_state["options_inloggning_2"] == "Registrera ny användare":
+            dev_login_page.custom_register_user()
+        if st.session_state["options_inloggning_2"] == "Glömt inloggningsuppgifter":
+            st.write("---")
+            options_credentials = ["Glömt lösenord", "Glömt användarnamn"]
+            st.radio("Glömt inloggningsuppgifter"
+                , options_credentials
+                , key="options_credentials_2"
+                , horizontal=True
+                , label_visibility="visible")
+            if st.session_state["options_credentials_2"] == "Glömt lösenord":
+                st.info("Ange ditt användarnamn så skickas ett lösenord\
+                        till den kopplade emailen.")
+                dev_login_page.custom_forgot_pw()
+            if st.session_state["options_credentials_2"] == "Glömt användarnamn":
+                st.info("Ange den kopplade emailen så visas användarnamnet.")
+                dev_login_page.custom_forgot_username()
