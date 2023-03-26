5cc8ebc99ed3dd2ccb93a0279f5bc9159b99d38f
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-03-26 13:26:18 +0200

    Update 01_inloggning.py

--- 01_inloggning.py
+++ 01_inloggning.py
@@ -47,6 +47,8 @@ if st.session_state["authentication_status"] == None and "random_cookie_name" no
 st.write("")
 st.write("---")
 st.write("")
+st.write("")
+st.write("")
 
 
 if st.session_state["authentication_status"]:
