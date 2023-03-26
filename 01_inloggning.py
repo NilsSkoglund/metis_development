968e8395f029c9b562f8c78b03007651046b7467
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-03-26 16:42:06 +0200

    Update 01_inloggning.py

--- 01_inloggning.py
+++ 01_inloggning.py
@@ -68,7 +68,7 @@ if st.session_state["authentication_status"]:
 
 
 if st.session_state["authentication_status"] != True:
-    co1, col2, col3 = st.columns([1,5,1])
+    co1, col2, col3 = st.columns([1,6,1])
     with col2:
 
         with st.expander("Visa övriga alternativ"):
