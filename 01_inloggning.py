ec4c1a08418e0b4714856694d2acb30b7cd11af7
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-03-26 13:19:37 +0200

    Update 01_inloggning.py

--- 01_inloggning.py
+++ 01_inloggning.py
@@ -6,12 +6,7 @@ from tools import dev_init_session_state_vars
 from tools import google_oauth
 import extra_streamlit_components as stx
 
-#centered
-st.markdown(
-    """<p style='display: block; text-align: center; font-size: 18px;'>Metis hjälpverktyg för lungemboli</p>
-    """,
-    unsafe_allow_html=True,
-)
+
 
 st.markdown("""
     <style>
@@ -20,6 +15,13 @@ st.markdown("""
     }
     </style>
     """,unsafe_allow_html=True)
+
+#centered
+st.markdown(
+    """<p style='display: block; text-align: center; font-size: 32px;'>Metis hjälpverktyg för lungemboli</p>
+    """,
+    unsafe_allow_html=True,
+)
 
 cm = stx.CookieManager(key="init2")
 
