d1b1d204787aa2c58bdeb0c4c2b36123bdf5c9be
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-04-21 17:02:13 +0200

    Update dev_login_page.py

--- tools/dev_login_page.py
+++ tools/dev_login_page.py
@@ -59,7 +59,7 @@ def custom_user_logged_in():
     st.markdown("<p style='text-align: center; font-size:30px'>Val av session</p>", unsafe_allow_html=True)
     dev_user_session_choice.start_new_session()
     st.write("---")
-    dev_user_session_choice.continue_most_recent_session()
+    dev_user_session_choice.choose_session_from_list()
 
     # options = ["Starta ny"
     #            , "Senaste"
